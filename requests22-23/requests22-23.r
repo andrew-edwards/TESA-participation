eb7b750028cf508c2ac9806990f8bf267ea6cd2f
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2022-06-07 09:04:28 -0700

    Update output to mostly match Mary's spreadsheet.

--- requests22-23/requests22-23.r
+++ requests22-23/requests22-23.r
@@ -1,5 +1,5 @@
 # requests22-23.r - import .csv file from Google Form (manually add a return at
-#  the end), and tidy up. Usually then just manually edit the spreadsheet. 24/6/22.
+#  the end), and tidy up. Usually then just manually edit the spreadsheet. 24/5/22.
 
 # requests19-20.r - import .csv file from Google Form (manually add a return at the end)
 #  and this code tidies it up. 11th June 2019.
@@ -17,41 +17,46 @@ require(dplyr)
 # stop("Do not re-run as am now manually working out the Priority for each course. Have resaved all-requests-comments.csv as PAC-TESA-requets-21.csv. Manually add to that now")
 # 34 responses
 tab_raw <- read.csv("TESA participation 2022-23.csv")
-tab <- tibble::as_tibble(tab_raw)
-
-tab <- dplyr::rename(tab,
-                     "Location" = "Work.location..to.estimate.travel.costs.",
-                     "HMM" =  "Hidden.Markov.Models",
-                     "sdmTMB" = "Spatial.and.spatiotemporal.models.with.sdmTMB",
-                     "Reproducible" =
-                       "Reproducible.science..best.practices.and.ICES.Transparent.Assessment.Framework",
-                     "PR" = "PR.Statistics..also.give.course.name.",
-                     "Highland" =
-                       "Highland.Statistics..also.give.course.name.",
-                     "Quebec" =
-                       "X.The.Quebec.Centre.for.Biodiversity.Science..also.give.course.name.",
-                     "Anadromous" = "Anadromous.Fishes.Stock.Assessment",
-                     "Ages" =
-                       "Best.practices.in.age.determination",
-                     "Liberating" =
-                       "Diving.Deeper.into.Workshop.and.Meeting.Facilitation.using.Liberating.Structures",
-#                     "sdmTMBYouTube" = "Spatiotemporal.modelling.with.sdmTMB...time.just.let.us.know.here.",   # May need tweaking
-                     "Notation" =
-                       "Some.guidance.on.using.mathematical.notation.in.biology",
-                     "Coding" = "Good.coding.practices",
-                     "RefPoints" = "Reference.points.101",
-                     "Indigenous" = "Indigenous.knowledge.and.stock.assessment.webinar.series"
-                     ) %>%
+tab <- tibble::as_tibble(tab_raw) %>%
+  mutate(Supervisor = NA,
+         # Region = "PAC",
+         Priority = NA) %>%
+  dplyr::relocate(
+           Surname,
+           First.name,
+           "Email" = "Username",
+           Division,
+           Supervisor,
+           Priority,
+           "Location" = "Work.location..to.estimate.travel.costs.",
+           "HMM" =  "Hidden.Markov.Models",
+           "sdmTMB" = "Spatial.and.spatiotemporal.models.with.sdmTMB",
+           "ICES.TAFReproducible" =
+             "Reproducible.science..best.practices.and.ICES.Transparent.Assessment.Framework",
+           "PRStats" = "PR.Statistics..also.give.course.name.",
+           "Highland" =
+             "Highland.Statistics..also.give.course.name.",
+           "PSBQ" =
+             "X.The.Quebec.Centre.for.Biodiversity.Science..also.give.course.name.",
+           "WS.anadromous" = "Anadromous.Fishes.Stock.Assessment",
+           "WS.ageing" =
+             "Best.practices.in.age.determination",
+           "WS.LibStr" =
+             "Diving.Deeper.into.Workshop.and.Meeting.Facilitation.using.Liberating.Structures",
+           "Webinar.sdmTMB" = "Spatiotemporal.modelling.with.sdmTMB..on.demand.YouTube.recordings...do.in.your.own.time.just.let.us.know.here.",
+           "Webinar.math" =
+             "Some.guidance.on.using.mathematical.notation.in.biology",
+           "Webinar.coding" = "Good.coding.practices",
+           "Webinar.RP101" = "Reference.points.101",
+           "Webinar.IKseries" = "Indigenous.knowledge.and.stock.assessment.webinar.series"
+         ) %>%
   select(-Timestamp)
 
 # Copy Lyanne's "Same answer as above":
 # tab[which(tab$First.name == "Lyanne"), "Ghement.advanced.ts"]  <-
 #   tab[which(tab$First.name == "Lyanne"), "Ghement.linear.ts"]
 
-tab = mutate(tab,
-             Region = "PAC",
-             Priority = NA)
-events = names(tab)[6:16]
+events = names(tab)[8:21]   # change manually
 
 # May need this based on 17-18 to shorten any long answers
 #tab = tbl_df(lapply(tab, function(x) {
@@ -81,24 +86,24 @@ for(i in 1:length(events)){
 #           select(noquote(paste0(events[i], "-tab")),
     #    assign(paste0(events[i], ".tab"), temp)
     if(nrow(temp) > 0){
-      # Individual files for each event (though in 2021 lots of events, so doing
-      # one big one
-      if(FALSE){
-        write.csv(file=paste0(events[i],
-                              ".csv"),
-                  temp,
-                  quote = FALSE,
-                  row.names = FALSE)   # Use this to decide priority
-        write.csv(file=paste0(events[i],
-                              "noComm.csv"),
-                  select(temp,
-                         Priority,
-                         Surname,
-                         First.name,
-                         Location),
-                  quote = FALSE,
-                  row.names = FALSE)         # Save this and then add in priorities manually
-      }
+      ## # Individual files for each event (though in 2021 lots of events, so doing
+      ## # one big one
+      ## if(FALSE){
+      ##   write.csv(file=paste0(events[i],
+      ##                         ".csv"),
+      ##             temp,
+      ##             quote = FALSE,
+      ##             row.names = FALSE)   # Use this to decide priority
+      ##   write.csv(file=paste0(events[i],
+      ##                         "noComm.csv"),
+      ##             select(temp,
+      ##                    Priority,
+      ##                    Surname,
+      ##                    First.name,
+      ##                    Location),
+      ##             quote = FALSE,
+      ##             row.names = FALSE)         # Save this and then add in priorities manually
+      ## }
 
       # Try just doing to one file:
       temp <- rbind(rep(",", length(names(temp))),
